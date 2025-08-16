5322bc27dc955aaa121bf718deeba3d94e842ce1
Emmy <ecr10.uk@gmail.com>
2025-08-16 16:08:36 +0100

    Change to description

--- src/components/Book/Pages/constants.ts
+++ src/components/Book/Pages/constants.ts
@@ -1,13 +1,10 @@
-export const ProfileDescription = `
-    University student and aspiring software engineer.\n
+export const ProfileDescription = `University student and aspiring software engineer.\n
     Fueled by caffeine, she wields logic to debug the foulest of code.\n
     On a quest to craft elegant programs and conquer the digital realm.\n
 `;
 
-export const LikesDescription = `
-    Crocheting, DnD & Sci-Fi books
+export const LikesDescription = ` Crocheting, DnD & Sci-Fi books
 `;
 
-export const DislikesDescription = `
-    Salted caramel, heights & spiders
+export const DislikesDescription = `Salted caramel, heights & spiders
 `;
